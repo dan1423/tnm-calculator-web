91030858cb1e2aab395094c7f4eda637ce2329a1
Daniel Oluwadare <oluwadare.daniel21@gmail.com>
2023-08-02 14:43:42 -0500

    style update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center   p-10" style={{ backgroundColor: '#fff', color: '#000000' }}>
       <h1 style={{ fontSize: 30 }}>AJCC TNM Staging Calculator</h1>
-      <div style={{ padding: 5, width: '100%'}} className='mb-5'>
+      <div  className='container mb-5'>
       <label  className='block mb-2 text-base font-medium text-gray-900 dark:text-white content-start'>Select a Staging Type:</label>
                 <select className='block w-6/12 px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg
                  bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 
